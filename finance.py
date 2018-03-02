d4d84879e094b4d1b6593799bc6175cdf70d3ac4
Alex <alex.e.rodgers@gmail.com>
2018-03-01 22:45:49 -0800

    Added a way to pull the history of each individual company

--- finance.py
+++ finance.py
@@ -6,7 +6,7 @@ from matplotlib import style
 import numpy as np
 import os
 import json
-#import pandas as pd
+import pandas as pd
 import pandas_datareader.data as web
 import pickle
 import requests
@@ -14,9 +14,9 @@ import csv
 from pprint import pprint
 
 # Setting up API
-series = ts(key=' I67BO05UHWQOTS4E')
-data = series.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
-print(data)
+series = ts(key='I67BO05UHWQOTS4E', output_format='pandas', indexing_type='date')
+data, metadata = series.get_daily_adjusted(symbol='MSFT', outputsize='full')
+#pprint(data.head(5))
 
 #Getting S&P ticker symbols
 def save_sp500_tickers():
@@ -45,6 +45,40 @@ def get_data(reload_sp500=False):
     if not os.path.exists('stock_dfs'):
         os.makedirs('stock_dfs')
 
+    for ticker in tickers:
+        print(ticker)
+        ticker.replace('.', '-')
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            data.to_csv('stock_dfs/{}.csv'.format(ticker))
+
+        else:
+            print('Already have {}'.format(ticker))
+
+#does not work
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count,ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('date', inplace=True)
+
+        df.rename(columns = {'Adj Close': ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.merge(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closed.csv')
 
 #save_sp500_tickers()
 #get_data()
+compile_data()
